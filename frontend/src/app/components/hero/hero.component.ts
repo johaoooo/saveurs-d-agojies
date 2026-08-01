d0255091e87f1054dd3b1040f6cff7a12537d2aa
johaoooo <josephdehazounde@gmail.com>
2026-08-01 23:27:59 +0100

    Hero: add left sliding image and right sliding image with 3-column layout

--- frontend/src/app/components/hero/hero.component.ts
+++ frontend/src/app/components/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,4 +8,29 @@ import { RouterLink } from '@angular/router';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
-export class HeroComponent {}
+export class HeroComponent implements OnInit, OnDestroy {
+  currentSlide = signal(0);
+  slides = [
+    'https://res.cloudinary.com/dzxesa3wi/image/upload/v1785622679/WhatsApp_Image_2026-08-01_at_23.13.40-removebg-preview_qyfecu.png',
+    'assets/images/plat-gourmet.jpg',
+    'assets/images/champ.jpg',
+  ];
+  leftSlides = [
+    'https://res.cloudinary.com/dzxesa3wi/image/upload/v1785622679/WhatsApp_Image_2026-08-01_at_23.13.40-removebg-preview_qyfecu.png',
+    'assets/images/hero-grill.jpg',
+    'assets/images/plat-chef.jpg',
+  ];
+  private timer: any;
+
+  ngOnInit(): void {
+    this.timer = setInterval(() => {
+      this.currentSlide.update((idx) => (idx + 1) % this.slides.length);
+    }, 4000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+}
