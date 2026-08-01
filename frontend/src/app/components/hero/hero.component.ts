fbd5d34746f50a8b13ac4c3c85128e9e8c9c20a5
johaoooo <josephdehazounde@gmail.com>
2026-08-01 23:49:04 +0100

    feat(hero): restreindre le carrousel aux 2 images Cloudinary et ajouter la navigation par puces

--- frontend/src/app/components/hero/hero.component.ts
+++ frontend/src/app/components/hero/hero.component.ts
@@ -13,15 +13,27 @@ export class HeroComponent implements OnInit, OnDestroy {
   slides = [
     'https://res.cloudinary.com/dzxesa3wi/image/upload/v1785612103/WhatsApp_Image_2026-08-01_at_20.13.49-removebg-preview_1_af8p7b.png',
     'https://res.cloudinary.com/dzxesa3wi/image/upload/v1785622679/WhatsApp_Image_2026-08-01_at_23.13.40-removebg-preview_qyfecu.png',
-    'assets/images/plat-gourmet.jpg',
-    'assets/images/champ.jpg',
   ];
   private timer: any;
 
   ngOnInit(): void {
+    this.startTimer();
+  }
+
+  setSlide(index: number): void {
+    this.currentSlide.set(index);
+    this.resetTimer();
+  }
+
+  private startTimer(): void {
     this.timer = setInterval(() => {
       this.currentSlide.update((idx) => (idx + 1) % this.slides.length);
     }, 4000);
+  }
+
+  private resetTimer(): void {
+    if (this.timer) clearInterval(this.timer);
+    this.startTimer();
   }
 
   ngOnDestroy(): void {
