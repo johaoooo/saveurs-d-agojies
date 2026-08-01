103d3b491601bb6b8c29975ddf7f1863dc0cf8e4
johaoooo <josephdehazounde@gmail.com>
2026-08-01 20:29:17 +0100

    Hero: infinite slide rotation with Cloudinary image

--- frontend/src/app/components/hero/hero.component.ts
+++ frontend/src/app/components/hero/hero.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
-export class HeroComponent {}
+export class HeroComponent implements OnInit, OnDestroy {
+  currentSlide = signal(0);
+  slides = [
+    'https://res.cloudinary.com/dzxesa3wi/image/upload/v1785612103/WhatsApp_Image_2026-08-01_at_20.13.49-removebg-preview_1_af8p7b.png',
+    'assets/images/plat-gourmet.jpg',
+    'assets/images/champ.jpg',
+  ];
+  private timer: any;
+
+  ngOnInit(): void {
+    this.timer = setInterval(() => {
+      this.currentSlide.update((idx) => (idx + 1) % this.slides.length);
+    }, 4000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+}
